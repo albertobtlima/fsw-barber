4ba47d73342bb911fbf64161870b60b9ff1beb54
Alberto Lima <alberto.btlima@outlook.com>
2024-08-07 09:49:37 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -3,33 +3,60 @@ import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 const Home = () => {
-   return (
-      <div>
-         <Header />
-         <div className="p-5">
-            <h2 className="text-xl font-bold">Olá, Fulano</h2>
-            <p>Segunda-feira, 05 de agosto.</p>
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Fulano</h2>
+        <p>Segunda-feira, 05 de agosto.</p>
 
-            <div className="mt-6 flex items-center gap-2">
-               <Input placeholder="Faça sua busca..." />
-               <Button>
-                  <SearchIcon />
-               </Button>
-            </div>
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
 
-            <div className="relative mt-6 h-[250px] w-full rounded-xl">
-               <Image
-                  alt="Agende nos melhores com FSW Barber"
-                  src="/banner-01.png"
-                  fill
-                  className="object-cover"
-               />
+        <div className="relative mt-6 h-[250px] w-full rounded-xl">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/banner-01.png"
+            fill
+            className="object-cover"
+          />
+        </div>
+
+        {/* AGENDAMENTO */}
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            {/* ESQUERDA */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
             </div>
-         </div>
+            {/* DIREITA */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">05</p>
+              <p className="text-sm">20:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
-   )
+    </div>
+  )
 }
 
 export default Home
